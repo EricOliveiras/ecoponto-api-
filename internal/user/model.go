e4bddb252213b8f704c87553b56bbec56a2420ea
Eric Oliveira <oiericoi@hotmail.com>
2025-11-11 11:10:42 -0300

    feat: add user model

---
+++ internal/user/model.go
@@ -0,0 +1,11 @@
+package user
+
+import "time"
+
+// User representa um usuário admin no banco
+type User struct {
+	ID           string    `db:"id" json:"id"`
+	Email        string    `db:"email" json:"email"`
+	PasswordHash string    `db:"password_hash" json:"-"`
+	CreatedAt    time.Time `db:"created_at" json:"created_at"`
+}
